c87dabf4f87a897c314d2321dd2f1d642613aeeb
SukhvirSingh1 <veer83594@gmail.com>
2026-01-27 17:44:36 +0530

    Add files via upload
    SQLite added

--- main.py
+++ main.py
@@ -1,58 +1,100 @@
-import json
 import tkinter as tk
 from tkinter import messagebox,simpledialog
 from datetime import date
+import sqlite3
 
 
-expenses = []
 
-class ExpenseTracker:
-    def __init__(self,filename='expense.json'):
-        self.filename = filename
-        self.expenses = self.load_expenses()
+
+def init_db():
+        conn = sqlite3.connect("expenses.db")
+        cur = conn.cursor()
+        
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS expenses(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            item TEXT,
+            amount REAL,
+            category TEXT,
+            date TEXT
+        )
+        """)
+        conn.commit()
+        conn.close()
+class ExpenseTracker:        
+            
+    # def __init__(self,filename='expense.json'):
+    #     self.filename = filename
+    #     self.expenses = self.load_expenses()
         
     # LOADING SYSTEM
       
     def load_expenses(self):
         try:
-            with open(self.filename, "r") as f:
-                return json.load(f)
+            conn = sqlite3.connect("expenses.db")
+            cur = conn.cursor()
+            
+            cur.execute("SELECT id, item, amount, category, date FROM expenses")
+            row = cur.fetchall()
+            conn.commit()
+            return [
+                {
+                    "id": r[0],
+                    "item": r[1],
+                    "amount": r[2],
+                    "category": r[3],
+                    "date": r[4]
+                }
+                for r in row
+            ]          
         except FileNotFoundError:
             return[]
     
-    def save_expenses(self):
-        with open(self.filename,"w") as f:
-            json.dump(self.expenses,f,indent=4)
+    # def save_expenses(self):
+    #     with open(self.filename,"w") as f:
+    #         json.dump(self.expenses,f,indent=4)
             
     # FUNCTIONS
         
     def add_expenses(self,item,amount,category,date):
-        self.expenses.append({
-            "item":item,
-            "amount":amount,
-            "category":category,
-            "date":date
-        })
-        self.save_expenses()
+        conn = sqlite3.connect("expenses.db")
+        cur = conn.cursor()
+        
+        cur.execute(
+            "INSERT INTO expenses (item, amount, category, date) VALUES (?, ?, ?, ?)",
+            (item, amount, category, date)
+            )
+        conn.commit()
+        conn.close()
+        
         print("Expense added!\n")
     
     def view_expenses(self):
-        if not self.expenses:
-            print("No expenses yet.\n")
-            return
+        conn = sqlite3.connect("expenses.db")
+        cur = conn.cursor()
         
-        print("\nYour Expenses")
-        for i,e in enumerate(self.expenses,start=1):
-            print(f"{i}:{e['item']} - {e['amount']} - {e['category']} - {e['date']}")
-        print()
-        
+        cur.execute("SELECT id, item, amount, category, date FROM expenses")
+        rows = cur.fetchall()
+        conn.close()
+        return rows
+    
+
     def total_expenses(self):
-        total = sum(e["amount"] for e in self.expenses)
+        expenses = tracker.view_expenses()
+        total = sum(e[2] for e in expenses)
         print(f"\nTotal spent:{total}\n")
+        
+    def dlt_expenses(self,expense_id):
+        conn=sqlite3.connect("expenses.db")
+        cur=conn.cursor()
+        cur.execute("DELETE FROM expenses WHERE id=?",(expense_id))
+        conn.commit()
+        conn.close()
         
         # MENU (GUI)
         
 tracker = ExpenseTracker()
+init_db()
 
 # GUI FUNCTIONS
 
@@ -79,38 +121,32 @@ def add_expense_gui():
     messagebox.showinfo("Success","Expense added!")
     
 def view_expenses_gui():
-    if not tracker.expenses:
+    expenses = tracker.view_expenses()
+    if not expenses:
         messagebox.showinfo("Expenses","No Expenses yet")
         return
         
     text = "Your Expenses\n\n"
-    for i,e in enumerate(tracker.expenses,start=1):
-        text += (f"{i}.{e['item']} - {e['amount']} - {e['category']} - {e['date']}\n")  
+    for i,e in enumerate(expenses,start=1):
+        text += (f"{i}.{e[1]} - {e[2]} - {e[3]} - {e[4]}\n")  
     messagebox.showinfo("Your Expenses",text)
     
 def sum_expenses_gui():
-    if not tracker.expenses:
+    expenses = tracker.view_expenses()
+    if not expenses:
         messagebox.showinfo("Expenses","No Expenses yet")
         return
-    total = sum(e["amount"] for e in tracker.expenses)
+    total = sum(e[2] for e in expenses)
     messagebox.showinfo("Sum of your expenses",total)
     
 def dlt_expenses_gui():
-    if not tracker.expenses:
-        messagebox.showinfo("Expenses","No expenses yet!")
+    expense_id = simpledialog.askstring("DELETE MENU","What you want to delete")
+    if not expense_id:
         return
-    try:
-        index = int(simpledialog.askstring("Epxense","What you want to delete:"))
-        if index < 1 or index > len(tracker.expenses):
-            messagebox.showinfo("Error","Expense NOt Found")
-            return
-    except(ValueError,TypeError):
-        return
-            
-    deleted = tracker.expenses.pop(index-1)
     
-    tracker.save_expenses()
-    messagebox.showinfo("Expenses",f"Your expense:{deleted['item']} - {deleted['amount']}")
+    tracker.dlt_expenses(expense_id)    
+    
+    messagebox.showinfo("Expenses",f"Your expense with id {expense_id} deleted")
     
 def monthly_filter_gui():
     if not tracker.expenses:
